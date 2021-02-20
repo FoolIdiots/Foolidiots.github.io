26e66d05d0e3f600ff44873a66d8f06de1ed4a5c
FoolIdiots <in2ovo@yeah.net>
2021-02-20 11:41:44 +0800

    Update

---
+++ html/vue.config.js
@@ -0,0 +1,11 @@
+chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+        config.output.filename('js/[name].js').end();
+        config.output.chunkFilename('js/[name].js').end();
+
+        config.plugin('extract-css').tap(args => [{
+            filename: `css/[name].css`,
+            chunkFilename: `css/[name].css`
+        }])
+    }
+},
